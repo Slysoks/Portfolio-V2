49eb7af2cc84519c04ba9a287b753f28821e4f6e
SupRMinecraftien <superminecraftien.levrai@gmail.com>
2024-07-20 11:01:59 +0200

    Added a scroll arrow Changed nav colors

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Todo:
+
+Change the header nav colors according to the scroll level (and with the arrows for sure )
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function randomHeaderNavLinkColor() {
-    let linksHolder = document.querySelectorAll('.randomLinkColorHolder');
-    // Console log the childrens
-    console.log(linksHolder);
+function navLinkColor() {
+    let li = document.querySelectorAll('.navColor');
+
     let colors = [
         'F8F7FF',
         '46B1C9',
@@ -11,27 +10,14 @@ function randomHeaderNavLinkColor() {
     ];
 
     let links = [];
-    
-    for (let i = 0; i < linksHolder.length; i++) {
-        linksHolder[i].childNodes.forEach(child => {
-            child.childNodes.forEach(child => {
-                if (child.tagName === 'A') {
-                    links.push(child);
-                }
-            });
-        });
-        console.log(links);
-    }
 
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener('mouseover', () => {
-            links[i].style.color = `#${colors[i]}`;
-            links[i].style.setProperty('--pseudo-background-color', `#${colors[i]}`);
-        });
-        links[i].addEventListener('mouseout', () => {
-            links[i].style.color = `#${colors[0]}`;
-        });
-    }
+    li.forEach((element, index) => {
+        if (index === window.scrollY / window.innerHeight) {
+            element.style.color = `#${colors[index]}`;
+        } else {
+            element.style.color = 'white';
+        }
+    });
 
 }
 
@@ -46,5 +32,60 @@ function setAge() {
     document.getElementById('age').innerHTML = age;
 }
 
-randomHeaderNavLinkColor();
+window.addEventListener('mousemove', function (e) {
+    // The goal is to navigate between the different section by clicking on the arrows
+
+    // Register the arrows
+    let arrow = document.getElementById('arrow');
+
+    if (this.window.innerWidth - e.clientX > 85 || e.clientY < 125) {
+        arrow.style.display = 'none';
+    } else {
+        arrow.style.display = 'flex';
+    }
+
+    // Register the current section using the scrollY value
+    let currentSectionIndex = Math.round(window.scrollY / window.innerHeight);
+
+    // Adjust the arrow position based on the cursor height
+    let arrowPosition = event.clientY;
+    arrow.style.top = `${arrowPosition - 25}px`;
+
+    checkArrowRotation();
+
+});
+
+document.getElementById('arrow').addEventListener('click', function () {
+    let arrow = document.getElementById('arrow');
+
+    let currentSection = window.scrollY / window.innerHeight;
+    let currentSectionIndex = Math.round(currentSection);
+
+    if (arrow.style.transform === 'rotate(180deg)') {
+        window.scrollTo({
+            top: (currentSectionIndex + 1) * window.innerHeight - 100,
+            behavior: 'smooth'
+        });
+    } else {
+        window.scrollTo({
+            top: (currentSectionIndex - 1) * window.innerHeight -100,
+            behavior: 'smooth'
+        });
+    }
+
+    checkArrowRotation();
+
+});
+
+function checkArrowRotation() {
+    let arrow = document.getElementById('arrow');
+    let currentSectionIndex = Math.round(window.scrollY / window.innerHeight);
+
+    if (currentSectionIndex === 0) arrow.style.transform = 'rotate(180deg)';
+    else if (event.clientY > (this.window.innerHeight + 100) / 2) arrow.style.transform = 'rotate(180deg)';
+    else arrow.style.transform = 'rotate(0deg)';
+    navLinkColor();
+}
+
+navLinkColor();
 setAge();
